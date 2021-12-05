2cab9c20ec83710b2e6968004c7204edf8380dff
Aleks <priozernoe@mail.ru>
2021-12-05 20:19:22 +0300

    add sample, add data in project

--- library/bboard/admin.py
+++ library/bboard/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Bb, Rubric
 
-# Register your models here.
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'auther', 'genre', 'status', 'data', 'give_out', 'give_in')
+    list_display_links = ('title', 'auther', 'genre')
+    search_fields = ('title', 'auther', 'genre',)
+
+
+admin.site.register(Bb, BbAdmin)
+admin.site.register(Rubric)
+
--- library/bboard/models.py
+++ library/bboard/models.py
@@ -1,13 +1,30 @@
 from django.db import models
 from django.forms import DateInput
-from datetime import datetime
 
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50)
-    auther = models.CharField(max_length=50)
-    genre = models.TextField(null=True, blank=True)
-    status = models.CharField(max_length=50)
-    data = models.CharField(max_length=50)
-    give_out = models.CharField(max_length=50)
-    give_in = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name='Наименование')
+    auther = models.CharField(max_length=50, verbose_name='Автор')
+    genre = models.CharField(max_length=50, verbose_name='Жанр')
+    status = models.TextField(null=True, blank=True, verbose_name='Описание')
+    data = models.CharField(max_length=50, verbose_name='Данные абонента')
+    give_out = models.CharField(max_length=50, verbose_name='Дата выдачи')
+    give_in = models.CharField(max_length=50, verbose_name='Дата возврата')
+    #rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
+
+    class Meta:
+        verbose_name_plural = 'База данных'
+        verbose_name = 'Поле данных'
+        ordering = ['-give_in']
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Статус')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Статус'
+        ordering = ['name']
